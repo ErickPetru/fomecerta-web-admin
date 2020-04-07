b23b21db43958890b52edc9d3d9dde70097268b8
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-04-07 16:21:08 -0300

    Removidas Cloud Functions de triggers do Firestore

--- functions/index.js
+++ functions/index.js
@@ -5,20 +5,3 @@ if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'nuxtSSR') {
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'grantCustomRole') {
   exports.grantCustomRole = require('./grantCustomRole');
 }
-
-// TODO: Re-habilitar a Cloud Functions quando conseguir fazer funcionar.
-// if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'establishmentTypesWrite') {
-//   exports.establishmentTypesWrite = require('./establishmentTypesWrite');
-// }
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'establishmentsWrite') {
-  exports.establishmentsWrite = require('./establishmentsWrite');
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'menuCategoriesWrite') {
-  exports.menuCategoriesWrite = require('./menuCategoriesWrite');
-}
-
-if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'menuItemsWrite') {
-  exports.menuItemsWrite = require('./menuItemsWrite');
-}
